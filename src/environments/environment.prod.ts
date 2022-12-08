2fa41817250e5382e62f3822d7e779beabdc987d
andreeatalpos <andreeatalpos2000@yahoo.com>
2022-12-08 18:03:45 +0200

    Assignment2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://20.79.100.213:8080'
+  apiUrl: 'http://localhost:8080'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://20.79.100.213:8080'
+  apiUrl: 'http://localhost:8080'
 
 };
 
